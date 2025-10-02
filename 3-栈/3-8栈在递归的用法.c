ea443ec88287a9baf08604e5b6400526bba83094
小帅 <1187961189@qq.com>
2025-10-02 18:46:28 +0800

    data

--- 3-栈/3-8栈在递归的用法.c
+++ 3-栈/3-8栈在递归的用法.c
@@ -1,13 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int fun(int x){
-    if(x == 0 || x == 1){
-        return 1;
-    }else{
-        // printf("%d\n");
-        return x * fun(x-1);
+void fun_tree(int x, int depth, int is_last) {
+    // 打印缩进和分支符号
+    for(int i = 0; i < depth - 1; i++) {
+        printf("│   ");
+    }
+    if(depth > 0) {
+        if(is_last)
+            printf("└──");
+        else
+            printf("├──");
+    }
+
+    // 打印当前节点
+    printf("fun(%d)\n", x);
+
+    if(x == 0 || x == 1) {
+        for(int i = 0; i < depth; i++) {
+            printf("    ");
+        }
+        printf("return 1\n");
+    } else {
+        // 递归调用
+        fun_tree(x - 1, depth + 1, 1);
+        for(int i = 0; i < depth; i++) {
+            printf("    ");
+        }
+        printf("return %d * fun(%d) = %d\n", x, x - 1, x * 1); // 最终返回值可以再用变量存，暂时显示计算公式
     }
 }
-int main(){
-    printf("%d",fun(4));
+
+int main() {
+    int n = 4;
+    fun_tree(n, 0, 1);
+    return 0;
 }
